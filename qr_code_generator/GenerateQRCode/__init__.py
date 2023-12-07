0fa4e3cbcea108b59ee51f28c22d682636139d5e
Rishab Kumar <rishabkumar7@gmail.com>
2023-12-07 10:50:02 +0530

    Added Azure Function files

---
+++ qr_code_generator/GenerateQRCode/__init__.py
@@ -0,0 +1,55 @@
+import os
+import io
+import uuid
+import qrcode
+import base64
+import logging
+from azure.storage.blob import BlobClient
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    url = req.params.get('url')
+    if not url:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            url = req_body.get('url')
+
+    if url:
+        # Create a QR code
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(url)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
+        img_base64 = base64.b64encode(img_byte_arr).decode('utf-8')
+
+        # Upload the QR code to Azure Blob Storage
+        connection_string = os.environ['AzureWebJobsStorage']
+        container_name = os.environ.get('BLOB_CONTAINER_NAME', 'qrcodes')
+        blob_name = str(url) + '.png'
+
+        blob_client = BlobClient.from_connection_string(connection_string, container_name, blob_name)
+        blob_client.upload_blob(img_byte_arr)
+
+        # Return the QR code image and URL
+        return func.HttpResponse(
+            f"QR code generated and saved to Azure Blob Storage!\nBlob URL: {blob_client.url}\n",
+            headers={"Content-Type": "text/plain"},
+        )
+
+    else:
+        return func.HttpResponse(
+            "Please pass a url on the query string or in the request body",
+            status_code=400
+        )
