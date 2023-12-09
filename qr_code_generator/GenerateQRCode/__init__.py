41f484ee9b12a22659109e806dd33cacd823d4f6
Rishab Kumar <rishabkumar7@gmail.com>
2023-12-09 15:40:48 +0530

    update Azure Function

--- qr_code_generator/GenerateQRCode/__init__.py
+++ qr_code_generator/GenerateQRCode/__init__.py
@@ -1,14 +1,10 @@
 import os
-import io
-import uuid
 import qrcode
-import base64
-import logging
-from azure.storage.blob import BlobServiceClient
+import io
 import azure.functions as func
+from azure.storage.blob import BlobServiceClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
     url = req.params.get('url')
     if not url:
         try:
@@ -19,27 +15,32 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             url = req_body.get('url')
 
     if url:
-        # Create a QR code
+        # Generate QR code
         qr = qrcode.QRCode(
             version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
             box_size=10,
             border=4,
         )
         qr.add_data(url)
         qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
-        img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format='PNG')
-        img_byte_arr = img_byte_arr.getvalue()
-        img_base64 = base64.b64encode(img_byte_arr).decode('utf-8')
 
-        # Upload the QR code to Azure Blob Storage
-        connection_string = os.environ['StorageConnection']
+        img = qr.make_image(fill='black', back_color='white')
+
+        # Convert the image to a byte array
+        byte_arr = io.BytesIO()
+        img.save(byte_arr, format='PNG')
+        img_byte_arr = byte_arr.getvalue()
+
+        # Get Azure Blob Storage details
+        connection_string = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
         container_name = os.environ.get('BLOB_CONTAINER_NAME', 'qr-codes')
         blob_name = str(url) + '.png'
 
-        blob_client = BlobServiceClient.from_connection_string(conn_str=connection_string,container_name=container_name)
+        # Create a blob client using the local file name as the name for the blob
+        blob_client = BlobServiceClient.from_connection_string(connection_string).get_blob_client(container_name, blob_name)
+
+        # Upload the created file
         blob_client.upload_blob(img_byte_arr)
 
         # Return the QR code image and URL
